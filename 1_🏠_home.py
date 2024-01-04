9b1771f50afaed5578b509f5219c8baa2f7d7eee
daviventura <155470809+daviventura@users.noreply.github.com>
2024-01-04 11:27:23 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -3,19 +3,6 @@ import pandas as pd
 import webbrowser
 from datetime import datetime
 
-if 'data' not in st.session_state:
-    df_data=pd.read_csv('datasets\CLEAN_FIFA23_official_data.csv',index_col=0)
-    df_data=df_data[df_data['Contract Valid Until']>=datetime.today().year]
-    df_data=df_data[df_data['Value(£)']>0]
-    df_data=df_data.sort_values(by='Overall',ascending=False)
-    st.session_state['data']=df_data
-
-
-
-df_data=pd.read_csv('datasets\CLEAN_FIFA23_official_data.csv',index_col=0)
-df_data.info()
-df_data.head(3)
-
 st.markdown('# FIFA 2023 OFICIAL DATASET ⚽')
 st.sidebar.markdown('Dados disponíveis no [Kaggle](https://www.kaggle.com/datasets/abecklas/fifa-world-cup)')
 
